42c7c9eccc5744bfb08120df29120b0d0c1a80be
lucarlig <lucarlig@protonmail.com>
2021-11-08 10:25:33 +0400

    initialize function and test

--- programs/snip/src/lib.rs
+++ programs/snip/src/lib.rs
@@ -6,9 +6,29 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod snip {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_snips = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 10000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_snips: u64,
+    pub snips_list: Vec<SnipStruct>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct SnipStruct {
+    pub snip_text: String,
+    pub user_address: Pubkey,
+}
--- tests/snip.ts
+++ tests/snip.ts
@@ -1,17 +1,36 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Snip } from '../target/types/snip';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Snip } from "../target/types/snip";
+import * as assert from "assert";
+const { SystemProgram } = anchor.web3;
 
-describe('snip', () => {
-
+describe("snip", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Snip as Program<Snip>;
+  const baseAccount = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    let tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
     console.log("Your transaction signature", tx);
+
+    // Get the account.
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    // Check that values are initialized
+    assert.deepEqual(account.totalSnips.toString(), "0");
+    assert.deepEqual(account.snipsList, []);
   });
 });
